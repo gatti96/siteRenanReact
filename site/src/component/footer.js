a4242c31968359239325c8b1fb349f904b6916bd
Renan Gatti <107943147+gatti96@users.noreply.github.com>
2023-03-09 19:54:21 -0300

    create Footer.js

---
+++ site/src/component/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render () {
+    return (
+      <>
+        <footer class="bg-new footer-1 text-center text-lg-start">
+        
+          <div class="text-center p-3">© 2022 Copyright: Renan Gatti</div>
+        
+        </footer>
+      </>
+    )
+  }
+}
+
+export default Footer;
